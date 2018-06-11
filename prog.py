bbb338b658c148b3e8b88b05f6cf89c480a31761
willl4 <williamlaycock@hanovernorwichschools.org>
2018-06-11 14:31:33 -0400

    Update prog.py

--- prog.py
+++ prog.py
@@ -45,7 +45,7 @@ for i in range(0,range1):
 n=0
 derivs.append(fval)
 
-for i in range(0,order+1):
+for i in range(0,order):
     fval = []
     valfval = 0.0
     for i in range(0,len(derivs[n])-1):
@@ -66,68 +66,28 @@ d=0
 ci = []
 for i in range(0,len(fn)):
     l = fn[i]/(math.factorial(i))
-    ci.append(f)
+    ci.append(l)
 
 tl = []
 for i in range(0,len(fn)):
-    tl = str("(x-")+str(center)+str(")**")+str(i)
-"""
-    fi = str("f")+str(i)+str(": ")
-    fl = str(fi)+str(l)
-    
-#calculates values of each order's taylor coeffecient using consecutive derivatives at the center and the factorial denominator 
-I0 = fval[1000]/1
-I1 = onederiv[1000]/1
-I2 = twoderiv[1000]/2
-I3 = threederiv[1000]/6
-I4 = fourderiv[1000]/24
-I5 = fivederiv[1000]/120
-I6 = sixderiv[1000]/720
-I7 = sevenderiv[1000]/5040
-I8 = eightderiv[1000]/40320
-I9 = ninederiv[1000]/362880
-I10 = tenderiv[1000]/3628800
-
-C0 = str(fval[1000]/1)
-C1 = str(onederiv[1000]/1)
-C2 = str(twoderiv[1000]/2)
-C3 = str(threederiv[1000]/6)
-C4 = str(fourderiv[1000]/24)
-C5 = str(fivederiv[1000]/120)
-C6 = str(sixderiv[1000]/720)
-C7 = str(sevenderiv[1000]/5040)
-C8 = str(eightderiv[1000]/40320)
-C9 = str(ninederiv[1000]/362880)
-C10 = str(tenderiv[1000]/3628800)
-
-print("Coefficients of each order:")
-print(str("C0: ")+str(C0))
-print(str("C1: ")+str(C1))
-print(str("C2: ")+str(C2))
-print(str("C3: ")+str(C3))
-print(str("C4: ")+str(C4))
-print(str("C5: ")+str(C5))
-print(str("C6: ")+str(C6))
-print(str("C7: ")+str(C7))
-print(str("C8: ")+str(C8))
-print(str("C9: ")+str(C9))
-print(str("C10: ")+str(C10))
-
-tl = []
-for i in range(0,len(fn)):
-    tlv = str("(x-")+str(center)+str(")**")+str(i)
+    if i == len(fn)-1:
+        tlv = str("(")+str(ci[i])+str("*(x-")+str(center)+str(")**")+str(i)+str(")")
+    else:
+        tlv = str("(")+str(ci[i])+str("*(x-")+str(center)+str(")**")+str(i)+str(")+")
     tl.append(tlv)
+eq = 1
+for i in range(0,len(fn)):
+    if i == 0:
+        eq = str(tl[i])
+    else:
+        eq = str(eq) + str(tl[i])
 
-tlval = str(" ")
-for i in range(0,len(tl)):
-    tfin = str(tl[i]) + tlval 
-    if len(tl) != 1 and i != len(tl)-1:
-        tfin = tfin + str("+")
-    tlval = str(tl[i])
-print(tfin)
+print(eq)
 
+fvalue = []
+for i in range(0,len(fn)):
+    valuef=ci[i]*((value-center)**i)
+    fvalue.append(valuef)
 
-#calculates value of estimation with Taylor's formula
-fvalue = I0*((value-center)**0)+I1*((value-center)**1)+I2*((value-center)**2)+I3*((value-center)**3)+I4*((value-center)**4)+I5*((value-center)**5)+I6*((value-center)**6)+I7*((value-center)**7)+I8*((value-center)**8)+I9*((value-center)**9)+I10*((value-center)**10)
-
-print(str("f(")+str(value)+str("): ") + str(fvalue))"""
+estimate = sum(fvalue)
+print(str("f(")+str(value)+str("): ")+str(estimate))
